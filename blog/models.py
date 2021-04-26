2f181169dc40cbca5c258eef238a0650badd0ad9
Victor Augusto <victor.augusto60@gmail.com>
2021-04-26 00:07:34 -0300

    Criado view para publicar Posts
    close #13

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,17 @@
-from datetime import datetime
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True, editable=False)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.published_date = datetime.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
